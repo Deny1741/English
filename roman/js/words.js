5fc4f2580d751c987f1eb774cc3f9c726d8d6fce
deny17 <clever1174@mail.ru>
2020-02-02 02:32:53 +0500

    1

--- roman/js/words.js
+++ roman/js/words.js
@@ -1,15 +1,16 @@
-export let wordsRus1 = ['колесо', 'Мотор', 'пот', 'гусеница', 'хвост', 'Сантехник', 'попугай',
+
+ export let wordsRus1 = ['колесо', 'Мотор', 'пот', 'гусеница', 'хвост', 'Сантехник', 'попугай',
     'уголь', 'ствол дерева', 'корзина', 'куст', 'коготь', 'пламя', 'кулак', 'дупло', 'посылка', 'кирпич', 'чайник', 'ведро',
     'камень'];
 
-export let wordsEng1 = ['wheel', 'engine', 'sweat', 'caterpillar', 'tail', 'plumber', 'parrot',
-    'coal', 'trunk', 'basket', 'bush', 'claw', 'flame', 'fist', 'hollow', 'parcel', 'brick', 'kettle', 'bucket',
-    'stone'];
+ export let wordsEng1 = ['wheel', 'engine', 'sweat', 'caterpillar', 'tail', 'plumber', 'parrot',
+        'coal', 'trunk', 'basket', 'bush', 'claw', 'flame', 'fist', 'hollow', 'parcel', 'brick', 'kettle', 'bucket',
+        'stone'];
 
-export let wordsRus2 = ['замок', 'треугольник', 'провод', 'парик', 'мыло', 'закат', 'созвездие', 'мел', 'кошелёк', 'грабитель',
-    'енот', 'корона', 'язык', 'коса из волос', 'забастовка', 'тренер', 'библиотека',
-    'борода', 'невеста', 'засуха'];
+ export let wordsRus2 = ['замок', 'треугольник', 'провод', 'парик', 'мыло', 'закат', 'созвездие', 'мел', 'кошелёк', 'грабитель',
+        'енот', 'корона', 'язык', 'коса из волос', 'забастовка', 'тренер', 'библиотека',
+        'борода', 'невеста', 'засуха'];
 
-export let wordsEng2 = ['castle', 'triangle', 'wire', 'wig', 'soap', 'sunset', 'constellation', 'chalk', 'purse', 'robber',
+ export let wordsEng2 = ['castle', 'triangle', 'wire', 'wig', 'soap', 'sunset', 'constellation', 'chalk', 'purse', 'robber',
     'raccoon', 'crown', 'tongue', 'plait', 'strike', 'coach', 'library',
     'beard', 'fiancee', 'drought'];
